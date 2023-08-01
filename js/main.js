7bfb079c8b938aaf1a63ad52477acc3702ebfa7a
Erick <erick.kuwahara79@gmail.com>
2023-07-31 22:18:04 -0300

    style: main layout

--- js/main.js
+++ js/main.js
@@ -1,110 +1,110 @@
 const apiKey = "b108b29c59782202c55af454c861a3b6";
 
-const placeLocation = document.getElementById('location');
+const placeLocation = document.getElementById("location");
 
-const weatherIcon = document.getElementById('weatherIcon');
-const weatherName = document.getElementById('weatherName');
-const weather = document.getElementById('weather');
+const weatherIcon = document.getElementById("weatherIcon");
+const weatherName = document.getElementById("weatherName");
+const weather = document.getElementById("weather");
 
-const dataWind = document.getElementById('dataWind');
-const dataHumidity = document.getElementById('dataHumidity');
+const dataWind = document.getElementById("dataWind");
+const dataHumidity = document.getElementById("dataHumidity");
 
-const cityInput = document.getElementById('cityInput');
-const searchCity = document.getElementById('searchCity');
+const cityInput = document.getElementById("cityInput");
+const searchCity = document.getElementById("searchCity");
 
-const date = document.getElementById('date')
-const today = new Date;
-date.innerText = today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear();
+const date = document.getElementById("date");
+const today = new Date();
+date.innerText = today.getDate() + "/" + today.getMonth();
 
 // DEFAULT VALUES
 
 window.onload = () => {
-    // PRE LOADER
-    const layer = document.getElementById('layer');
-    const body = document.querySelector('body');
+  // PRE LOADER
+  const layer = document.getElementById("layer");
+  const body = document.querySelector("body");
 
-    setTimeout(() => {
-        body.style.overflowY = "visible"
-        layer.style.opacity = '0';
+  setTimeout(() => {
+    body.style.overflowY = "visible";
+    layer.style.opacity = "0";
 
-        if (layer.style.opacity == 0) {
-            setTimeout(() => {
-                layer.style.display = "none";
-            }, 500)
-        }
-    }, 1500)
+    if (layer.style.opacity == 0) {
+      setTimeout(() => {
+        layer.style.display = "none";
+      }, 500);
+    }
+  }, 1500);
 
-    // DEFAULT VALUES
+  // DEFAULT VALUES
 
-    let city = "Santos";
-    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&units=metric`;
+  let city = "Santos";
+  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&units=metric`;
 
-    fetch(apiUrl)
-        .then(response => {
-            return response.json();
-        })
-        .then(body => {
-            obj = body
-            getDataCity();
-        })
-}
+  fetch(apiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((body) => {
+      obj = body;
+      getDataCity();
+    });
+};
 
-// DYNAMIC 
+// DYNAMIC
 
 function getCity() {
-    const city = cityInput.value;
-    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&units=metric`;
+  const city = cityInput.value;
+  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&units=metric`;
 
-    fetch(apiUrl)
-        .then(response => {
-            return response.json();
-        })
-        .then(body => {
-            obj = body
-            getDataCity();
-        })
+  fetch(apiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((body) => {
+      obj = body;
+      getDataCity();
+    });
 }
 
 function getDataCity() {
-    const cityData = obj;
-    const longitude = cityData[0].lon;
-    const latitude = cityData[0].lat;
+  const cityData = obj;
+  const longitude = cityData[0].lon;
+  const latitude = cityData[0].lat;
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
-    fetch(apiUrl)
-        .then(response => {
-            return response.json();
-        })
-        .then(body => {
-            obj = body
-            showDataCity();
-        })
+  fetch(apiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((body) => {
+      obj = body;
+      showDataCity();
+    });
 }
 
 function showDataCity() {
-    const selectedCity = obj;
+  const selectedCity = obj;
 
-    const cityName = selectedCity.name;
-    const countryName = selectedCity.sys.country;
-    const temperature = Math.round(selectedCity.main.temp - 273) + "º";
-    const wind = selectedCity.wind.speed + " km/h";
-    const humidity = selectedCity.main.humidity + "%";
-    const icon = selectedCity.weather[0].icon;
-    const name = selectedCity.weather[0].description;
+  const cityName = selectedCity.name;
+  const countryName = selectedCity.sys.country;
+  const temperature = Math.round(selectedCity.main.temp - 273) + "º";
+  const wind = selectedCity.wind.speed + " km/h";
+  const humidity = selectedCity.main.humidity + "%";
+  const icon = selectedCity.weather[0].icon;
+  const name = selectedCity.weather[0].description;
 
-    placeLocation.innerText = cityName + " | " + countryName;
-    weather.innerText = temperature;
-    dataWind.innerText = wind;
-    dataHumidity.innerText = humidity;
+  placeLocation.innerText = cityName + ", " + countryName;
+  weather.innerText = temperature;
+  dataWind.innerText = wind;
+  dataHumidity.innerText = humidity;
 
-    weatherIcon.src = `./images/${icon}.png`;
-    weatherName.innerText = name;
+  weatherIcon.src = `./images/${icon}.png`;
+  weatherName.innerText = name;
 }
 
-searchCity.addEventListener('click', getCity);
-cityInput.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter') {
-        getCity();
-    }
-})
+searchCity.addEventListener("click", getCity);
+cityInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    getCity();
+  }
+});
